d4120c40c7de6ea4c2a94bd866dcbf0e978c7f8d
Richard <richardcck@hotmail.com>
2020-05-10 23:54:56 -0300

    aula 11

---
+++ aula11/a11-p1.py
@@ -0,0 +1,409 @@
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy as np
+import glm
+import math
+from PIL import Image
+
+glfw.init()
+glfw.window_hint(glfw.VISIBLE, glfw.FALSE);
+altura = 1600
+largura = 1200
+window = glfw.create_window(largura, altura, "Malhas e Texturas", None, None)
+glfw.make_context_current(window)
+
+vertex_code = """
+        attribute vec3 position;
+        attribute vec2 texture_coord;
+        varying vec2 out_texture;
+                
+        uniform mat4 model;
+        uniform mat4 view;
+        uniform mat4 projection;        
+        
+        void main(){
+            gl_Position = projection * view * model * vec4(position,1.0);
+            out_texture = vec2(texture_coord);
+        }
+        """
+
+fragment_code = """
+        uniform vec4 color;
+        varying vec2 out_texture;
+        uniform sampler2D samplerTexture;
+        
+        void main(){
+            vec4 texture = texture2D(samplerTexture, out_texture);
+            gl_FragColor = texture;
+        }
+        """
+# Request a program and shader slots from GPU
+program  = glCreateProgram()
+vertex   = glCreateShader(GL_VERTEX_SHADER)
+fragment = glCreateShader(GL_FRAGMENT_SHADER)
+
+# Set shaders source
+glShaderSource(vertex, vertex_code)
+glShaderSource(fragment, fragment_code)
+
+# Compile shaders
+glCompileShader(vertex)
+if not glGetShaderiv(vertex, GL_COMPILE_STATUS):
+    error = glGetShaderInfoLog(vertex).decode()
+    print(error)
+    raise RuntimeError("Erro de compilacao do Vertex Shader")
+
+glCompileShader(fragment)
+if not glGetShaderiv(fragment, GL_COMPILE_STATUS):
+    error = glGetShaderInfoLog(fragment).decode()
+    print(error)
+    raise RuntimeError("Erro de compilacao do Fragment Shader")
+
+# Attach shader objects to the program
+glAttachShader(program, vertex)
+glAttachShader(program, fragment)
+
+# Build program
+glLinkProgram(program)
+if not glGetProgramiv(program, GL_LINK_STATUS):
+    print(glGetProgramInfoLog(program))
+    raise RuntimeError('Linking error')
+    
+# Make program the default program
+glUseProgram(program)
+
+
+def load_model_from_file(filename):
+    """Loads a Wavefront OBJ file. """
+    objects = {}
+    vertices = []
+    texture_coords = []
+    faces = []
+
+    material = None
+
+    # abre o arquivo obj para leitura
+    for line in open(filename, "r"): ## para cada linha do arquivo .obj
+        if line.startswith('#'): continue ## ignora comentarios
+        values = line.split() # quebra a linha por espaço
+        if not values: continue
+
+
+        ### recuperando vertices
+        if values[0] == 'v':
+            vertices.append(values[1:4])
+
+
+        ### recuperando coordenadas de textura
+        elif values[0] == 'vt':
+            texture_coords.append(values[1:3])
+
+        ### recuperando faces 
+        elif values[0] in ('usemtl', 'usemat'):
+            material = values[1]
+        elif values[0] == 'f':
+            face = []
+            face_texture = []
+            for v in values[1:]:
+                w = v.split('/')
+                face.append(int(w[0]))
+                if len(w) >= 2 and len(w[1]) > 0:
+                    face_texture.append(int(w[1]))
+                else:
+                    face_texture.append(0)
+
+                faces.append((face, face_texture, material))
+
+    model = {}
+    model['vertices'] = vertices
+    model['texture'] = texture_coords
+    model['faces'] = faces
+
+    return model
+
+vertices_list = []
+
+#Processando modelo cube.obj. Vertice inicial: 0
+#Processando modelo cube.obj. Vertice final: 108
+modelo = load_model_from_file('cube.obj')
+### inserindo vertices do modelo no vetor de vertices
+print('Processando modelo cube.obj. Vertice inicial:',len(vertices_list))
+for face in modelo['faces']:
+    for vertice_id in face[0]: vertices_list.append( modelo['vertices'][vertice_id-1] )
+print('Processando modelo cube.obj. Vertice final:',len(vertices_list))
+
+modelo = load_model_from_file('sword.obj')
+### inserindo vertices do modelo no vetor de vertices
+print('Processando modelo sword.obj. Vertice inicial:',len(vertices_list))
+for face in modelo['faces']:
+    for vertice_id in face[0]: vertices_list.append( modelo['vertices'][vertice_id-1] )
+print('Processando modelo sword.obj. Vertice final:',len(vertices_list))
+
+modelo = load_model_from_file('plant.obj')
+### inserindo vertices do modelo no vetor de vertices
+print('Processando modelo plant.obj. Vertice inicial:',len(vertices_list))
+for face in modelo['faces']:
+    for vertice_id in face[0]: vertices_list.append( modelo['vertices'][vertice_id-1] )
+print('Processando modelo plant.obj. Vertice final:',len(vertices_list))
+
+modelo = load_model_from_file('cat.obj')
+### inserindo vertices do modelo no vetor de vertices
+print('Processando modelo cat.obj. Vertice inicial:',len(vertices_list))
+for face in modelo['faces']:
+    for vertice_id in face[0]: vertices_list.append( modelo['vertices'][vertice_id-1] )
+print('Processando modelo cat.obj. Vertice final:',len(vertices_list))
+
+vertices = np.zeros(len(vertices_list), [("position", np.float32, 3)])
+vertices['position'] = vertices_list
+
+def desenha_cubo():
+    
+    global vertices
+    
+    # aplica a matriz model
+    
+    # rotacao
+    angle = 0.0
+    r_x = 0.0; r_y = 0.0; r_z = 1.0
+    
+    # translacao
+    t_x = 0.0; t_y = 0.0; t_z = -5.0
+    
+    # escala
+    s_x = 1.0; s_y = 1.0; s_z = 1.0
+    
+    mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
+    loc_model = glGetUniformLocation(program, "model")
+    glUniformMatrix4fv(loc_model, 1, GL_TRUE, mat_model)
+       
+    # desenha o cubo
+    glDrawArrays(GL_TRIANGLES, 0, 108) ## renderizando
+    
+
+    
+def desenha_espada():
+    
+    global vertices
+    
+    # aplica a matriz model
+    
+    # rotacao
+    angle = 90.0
+    r_x = 0.0; r_y = 0.0; r_z = 1.0
+    
+    # translacao
+    t_x = .0; t_y = 0.0; t_z = .0
+    
+    # escala
+    s_x = .03; s_y = .03; s_z = .03
+    
+    mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
+    loc_model = glGetUniformLocation(program, "model")
+    glUniformMatrix4fv(loc_model, 1, GL_TRUE, mat_model)
+       
+    # desenha a espada
+    glDrawArrays(GL_TRIANGLES, 108, 20876-108) ## renderizando
+    
+    
+def desenha_planta():
+    
+    global vertices
+    
+    # aplica a matriz model
+    
+    # rotacao
+    angle = 0.0
+    r_x = 0.0; r_y = 0.0; r_z = 1.0
+    
+    # translacao
+    t_x = -3.0; t_y = -1.5; t_z = 1.5
+    
+    # escala
+    s_x = .2; s_y = .2; s_z = .2
+    
+    mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
+    loc_model = glGetUniformLocation(program, "model")
+    glUniformMatrix4fv(loc_model, 1, GL_TRUE, mat_model)
+       
+    # desenha a planta
+    glDrawArrays(GL_TRIANGLES, 20876, 387596-20876-180) ## renderizando
+    
+    
+def desenha_gato():
+    
+    global vertices
+    
+    # aplica a matriz model
+    
+    # rotacao
+    angle = 90.0
+    r_x = -1.0; r_y = 0.0; r_z = 0.0
+    
+    # translacao
+    t_x = .0; t_y = -5.0; t_z = 0.0
+    
+    # escala
+    s_x = .1; s_y = 0.1; s_z = 0.1
+    
+    mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
+    loc_model = glGetUniformLocation(program, "model")
+    glUniformMatrix4fv(loc_model, 1, GL_TRUE, mat_model)
+       
+    # desenha o gato
+    glDrawArrays(GL_TRIANGLES, 387596, 952204-387596-20876-180) ## renderizando
+
+# Request a buffer slot from GPU
+buffer = glGenBuffers(2)
+# Make this buffer the default one
+glBindBuffer(GL_ARRAY_BUFFER, buffer[0])
+
+# Upload data
+glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+glBindBuffer(GL_ARRAY_BUFFER, buffer[0])
+
+# Bind the position attribute
+# --------------------------------------
+stride = vertices.strides[0]
+offset = ctypes.c_void_p(0)
+
+loc = glGetAttribLocation(program, "position")
+glEnableVertexAttribArray(loc)
+
+glVertexAttribPointer(loc, 3, GL_FLOAT, False, stride, offset)
+
+loc_color = glGetUniformLocation(program, "color")
+
+cameraPos   = glm.vec3(0.0,  0.0,  1.0)
+cameraFront = glm.vec3(0.0,  0.0, -1.0)
+cameraUp    = glm.vec3(0.0,  1.0,  0.0)
+
+
+def key_event(window,key,scancode,action,mods):
+    global cameraPos, cameraFront, cameraUp
+    
+    cameraSpeed = 0.2
+    if key == 87 and (action==1 or action==2): # tecla W
+        cameraPos += cameraSpeed * cameraFront
+    
+    if key == 83 and (action==1 or action==2): # tecla S
+        cameraPos -= cameraSpeed * cameraFront
+    
+    if key == 65 and (action==1 or action==2): # tecla A
+        cameraPos -= glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
+        
+    if key == 68 and (action==1 or action==2): # tecla D
+        cameraPos += glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
+        
+firstMouse = True
+yaw = -90.0 
+pitch = 0.0
+lastX =  largura/2
+lastY =  altura/2
+
+def mouse_event(window, xpos, ypos):
+    global firstMouse, cameraFront, yaw, pitch, lastX, lastY
+    if firstMouse:
+        lastX = xpos
+        lastY = ypos
+        firstMouse = False
+
+    xoffset = xpos - lastX
+    yoffset = lastY - ypos
+    lastX = xpos
+    lastY = ypos
+
+    sensitivity = 0.3 
+    xoffset *= sensitivity
+    yoffset *= sensitivity
+
+    yaw += xoffset
+    pitch += yoffset
+
+    
+    if pitch >= 90.0: pitch = 90.0
+    if pitch <= -90.0: pitch = -90.0
+
+    front = glm.vec3()
+    front.x = math.cos(glm.radians(yaw)) * math.cos(glm.radians(pitch))
+    front.y = math.sin(glm.radians(pitch))
+    front.z = math.sin(glm.radians(yaw)) * math.cos(glm.radians(pitch))
+    cameraFront = glm.normalize(front)
+
+
+    
+glfw.set_key_callback(window,key_event)
+glfw.set_cursor_pos_callback(window, mouse_event)
+
+def model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z):
+    
+    angle = math.radians(angle)
+    
+    matrix_transform = glm.mat4(1.0) # instanciando uma matriz identidade
+       
+    # aplicando rotacao
+    matrix_transform = glm.rotate(matrix_transform, angle, glm.vec3(r_x, r_y, r_z))
+        
+  
+    # aplicando translacao
+    matrix_transform = glm.translate(matrix_transform, glm.vec3(t_x, t_y, t_z))    
+    
+    # aplicando escala
+    matrix_transform = glm.scale(matrix_transform, glm.vec3(s_x, s_y, s_z))
+    
+    matrix_transform = np.array(matrix_transform).T # pegando a transposta da matriz (glm trabalha com ela invertida)
+    
+    return matrix_transform
+
+def view():
+    global cameraPos, cameraFront, cameraUp
+    mat_view = glm.lookAt(cameraPos, cameraPos + cameraFront, cameraUp)
+    mat_view = np.array(mat_view)
+    return mat_view
+
+def projection():
+    global altura, largura
+    # perspective parameters: fovy, aspect, near, far
+    mat_projection = glm.perspective(glm.radians(45.0), largura/altura, 0.1, 1000.0)
+    mat_projection = np.array(mat_projection)    
+    return mat_projection
+
+glfw.show_window(window)
+glfw.set_cursor_pos(window, lastX, lastY)
+
+glEnable(GL_DEPTH_TEST) ### importante para 3D
+   
+
+
+while not glfw.window_should_close(window):
+
+    glfw.poll_events() 
+    
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    
+    glClearColor(1.0, 1.0, 1.0, 1.0)
+    
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE)
+    
+
+    desenha_cubo()   
+    desenha_espada()   
+    desenha_planta()   
+    desenha_gato()   
+
+    
+    mat_view = view()
+    loc_view = glGetUniformLocation(program, "view")
+    glUniformMatrix4fv(loc_view, 1, GL_FALSE, mat_view)
+
+    mat_projection = projection()
+    loc_projection = glGetUniformLocation(program, "projection")
+    glUniformMatrix4fv(loc_projection, 1, GL_FALSE, mat_projection)    
+    
+    
+
+    
+    glfw.swap_buffers(window)
+
+glfw.terminate()
